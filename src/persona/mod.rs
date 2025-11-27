1f1d3307339708bcf73f049e1a4c24670ee45751
JillStingray1 <jerryhouau@gmail.com>
2025-11-27 20:39:24 +0800

    started work on personas

--- src/persona/mod.rs
+++ src/persona/mod.rs
@@ -1,1 +1,2 @@
 pub mod arcana;
+pub mod persona;
---
+++ src/persona/persona.rs
@@ -0,0 +1,7 @@
+use super::arcana::Arcana;
+struct Persona {
+    arcana: Arcana,
+    base_level: i8,
+    affinites: [i8; 9],
+    inheritance: [i8; 13],
+}
