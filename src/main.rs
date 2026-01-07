7d21e48d878a032f7ac4749b81f73ea19f801f36
JillStingray1 <jerryhouau@gmail.com>
2026-01-07 23:14:52 +0800

    completed forward fusion logic

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,43 @@ mod persona;
 
 fn main() {
     use persona::arcana::Arcana::*;
-    println!("result: {:?}", Hermit - Fool)
+    println!("result: {:?}", Empress - Priestess);
+    let orpheus = persona::persona::Persona {
+        name: String::from("Orpheus"),
+        arcana: Fool,
+        base_level: 1,
+        affinities: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        inheritance: vec![],
+        skills: vec![],
+        cost: 0,
+        stats: [0, 0, 0, 0, 0],
+    };
+    let nekomata = persona::persona::Persona {
+        name: String::from("Nekomata"),
+        arcana: Magician,
+        base_level: 3,
+        affinities: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        inheritance: vec![],
+        skills: vec![],
+        cost: 0,
+        stats: [0, 0, 0, 0, 0],
+    };
+    let omoikane = persona::persona::Persona {
+        name: String::from("Omoikane"),
+        arcana: Hierophant,
+        base_level: 7,
+        affinities: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        inheritance: vec![],
+        skills: vec![],
+        cost: 0,
+        stats: [0, 0, 0, 0, 0],
+    };
+    let persona_db = vec![orpheus, nekomata, omoikane];
+
+    let result = persona_db[0].fuse(&persona_db[1], &persona_db);
+    println!("{}", result.unwrap().name);
+    let result_1 = persona_db[0].find_all_forward_fusions(&persona_db);
+    for (ingredient, result) in result_1 {
+        println!("Orpheus + {} => {}", ingredient.name, result.name);
+    }
 }
--- src/persona/persona.rs
+++ src/persona/persona.rs
@@ -1,24 +1,83 @@
-use std::ops::Add;
+use crate::persona;
 
 use super::arcana::Arcana;
 use super::skills::*;
 
 pub struct Persona {
-    name: String,
-    arcana: Arcana,
-    base_level: u8,
-    affinites: [u8; 10],
-    inheritance: Vec<SkillType>,
-    skills: Vec<(Skill, u8)>,
-    cost: u32,
-    stats: [u8; 5],
+    pub name: String,
+    pub arcana: Arcana,
+    pub base_level: u8,
+    pub affinities: [u8; 10],
+    pub inheritance: Vec<SkillType>,
+    pub skills: Vec<(Skill, u8)>,
+    pub cost: u32,
+    pub stats: [u8; 5],
 }
 
-impl Add for Persona {
-    type Output = Persona;
+impl Persona {
+    /**
+     * Gets the result of a fusion between 2 personae.
+     *
+     * This is determined by the using the fusion table to determine
+     * the resultant arcana of the fusion, and then finding the lowest
+     * level persona above the average level of the ingredients + 1
+     *
+     */
+    pub fn fuse<'a>(
+        &self,
+        rhs: &'a Self,
+        persona_list: &'a Vec<Self>,
+    ) -> Option<&'a Self> {
+        let fused_arcana = self.arcana + rhs.arcana;
 
-    fn add(self, rhs: Self) -> Self::Output {
-        let target = self.arcana + rhs.arcana;
-        todo!()
+        let mut result_persona: Option<&Self> = None;
+        if fused_arcana != self.arcana {
+            let fused_level = (self.base_level + rhs.base_level) / 2 + 1;
+            let mut result_level = 99;
+            for persona in persona_list {
+                println!("{}", persona.name);
+                if (persona.arcana == fused_arcana)
+                    && (persona.base_level >= fused_level)
+                    && (persona.base_level < result_level)
+                {
+                    println!("{:?}", fused_level);
+                    result_level = persona.base_level;
+                    result_persona = Some(persona)
+                }
+            }
+        } else {
+            let fused_level = (self.base_level + rhs.base_level) / 2 - 1;
+            for persona in persona_list {
+                let mut result_level = 0;
+                if (persona.arcana == fused_arcana)
+                    && (persona.base_level <= fused_level)
+                    && (persona.base_level > result_level)
+                {
+                    result_level = persona.base_level;
+                    result_persona = Some(persona)
+                }
+            }
+        }
+        result_persona
+    }
+
+    /**
+     * Returns a list of all possible forward fusions of some persona in a fusion
+     * database
+     *
+     *
+     */
+    pub fn find_all_forward_fusions<'a>(
+        &self,
+        persona_list: &'a Vec<Self>,
+    ) -> Vec<(&'a Self, &'a Self)> {
+        let mut forward_fusions = vec![];
+        for persona in persona_list {
+            match self.fuse(persona, persona_list) {
+                Some(x) => forward_fusions.push((persona, x)),
+                None => (),
+            }
+        }
+        forward_fusions
     }
 }
