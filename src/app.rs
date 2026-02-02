884b6c5c2124d31be81c0ecc71e9c573ab12fa55
JillStingray1 <jerryhouau@gmail.com>
2026-02-02 16:37:36 +0800

    completed route for persona data page

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,6 @@
-use crate::make_persona_db;
 use crate::persona::{Persona, Skill};
 use crate::templates::*;
+use crate::{make_persona_db, persona};
 use actix_web::web;
 use actix_web::{HttpRequest, HttpResponse, Responder, get, post, web::Path};
 
@@ -38,18 +38,23 @@ pub async fn persona_details(
     data: web::Data<AppData>,
 ) -> impl Responder {
     let persona_name = path.into_inner();
-    let mut target_persona: Option<&Persona> = None;
+    let mut searched_persona: Option<&Persona> = None;
     for persona in &data.persona_list {
         if persona.name == persona_name {
-            target_persona = Some(persona);
+            searched_persona = Some(persona);
         }
     }
-    let template = PersonaTemplate {
-        persona: target_persona.unwrap(),
-        forward_fusions: target_persona
-            .unwrap()
-            .find_all_forward_fusions(&data.persona_list),
-        special_fusion: None,
-    };
-    HttpResponse::Ok().body(template.render().unwrap())
+    match searched_persona {
+        Some(found_persona) => {
+            let template = PersonaTemplate {
+                persona: found_persona,
+                forward_fusions: found_persona
+                    .find_all_forward_fusions(&data.persona_list),
+                reverse_fusions: found_persona
+                    .find_all_reverse_fusions(&data.persona_list),
+            };
+            HttpResponse::Ok().body(template.render().unwrap())
+        }
+        None => HttpResponse::BadRequest().body("Persona does not exist"),
+    }
 }
