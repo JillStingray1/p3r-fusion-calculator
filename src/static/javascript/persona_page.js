ae880459da00c8970570d236b6a4afacb5a4f66b
JillStingray1 <jerryhouau@gmail.com>
2026-02-09 10:06:29 +0800

    added tabs to show/hide fusion tabels

--- src/static/javascript/persona_page.js
+++ src/static/javascript/persona_page.js
@@ -11,3 +11,16 @@ window.onload = function () {
         bInfo: false,
     });
 }
+
+function show_fusions(event, table_name) {
+    let tabs = document.getElementsByClassName("tabcontent");
+    for (let i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = "none";
+    }
+    let tab_buttons = document.getElementsByClassName("tabbutton active");
+    for (let i = 0; i < tab_buttons.length; i++) {
+        tab_buttons[i].classList.remove("active");
+    }
+    document.getElementById(table_name).style.display = "block";
+    event.currentTarget.classList.add("active");
+}
